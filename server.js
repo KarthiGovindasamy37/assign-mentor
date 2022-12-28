299ac69521249822e91ec6ec1989bc89506768ed
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2022-12-28 11:26:26 +0530

    Updated

--- server.js
+++ server.js
@@ -3,108 +3,198 @@ const app=express()
 const mongodb=require("mongodb")
 const mongoclient=mongodb.MongoClient
 const dotenv=require("dotenv").config()
+const cors=require("cors")
 
 let URL=process.env.URL
 let DB=process.env.DB
 
 app.use(express.json())
 
+app.use(cors({
+   origin:"http://localhost:3000"
+}))
+
+app.get("/students",async(req,res)=>{
+    try {
+        
+        let connection=await mongoclient.connect(URL);
+        
+        let db=connection.db(DB);
+        
+        let students=await db.collection("students").find().toArray();
+        
+        res.json(students)
+    await connection.close();
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong,please try again"})
+    }
+})
+
 
 app.post("/createstudent",async(req,res)=>{
     try {
-        
+       
         let connection=await mongoclient.connect(URL);
 
         let db=connection.db(DB);
 
-        await db.collection("student").insertOne(req.body);
-
-        await connection.close();
+        await db.collection("students").insertOne(req.body);
 
         res.json({message:"Student created"});
+
+        await connection.close();
     } catch (error) {
         res.status(500).json({message:"Somethig went wrong,try again"})
     }
 })
 
-app.post("/creatementor",async(req,res)=>{
+app.get("/student/:id",async(req,res)=>{
+    try {
+        let connection=await mongoclient.connect(URL)
+
+        let db=connection.db(DB)
+
+        let student=await db.collection("students").findOne({_id:mongodb.ObjectId(req.params.id)})
+
+        res.json(student)
+
+        await connection.close()
+    } catch (error) {
+        res.status(500).json({message:"Somethig went wrong,try again"})
+    }
+})
+
+app.put("/student/:id",async(req,res)=>{
+    try {
+        let connection=await mongoclient.connect(URL)
+
+        let db=connection.db(DB)
+
+        await db.collection("students").findOneAndUpdate({_id:mongodb.ObjectId(req.params.id)},{$set:req.body})
+
+        res.json({message:"Student updated successfully"})
+
+        await connection.close()
+    } catch (error) {
+        res.status(500).json({message:"Somethig went wrong,try again"})
+    }
+})
+
+app.delete("/student/:id",async(req,res)=>{
+    try {
+        let connection=await mongoclient.connect(URL)
+
+        let db=connection.db(DB)
+
+        await db.collection("students").findOneAndDelete({_id:mongodb.ObjectId(req.params.id)})
+
+        res.json({message:"Student deleted successfully"})
+
+        await connection.close()
+    } catch (error) {
+        res.status(500).json({message:"Somethig went wrong,try again"})
+    }
+})
+
+app.get("/teachers",async(req,res)=>{
     try {
         
+        let connection=await mongoclient.connect(URL);
+        
+        let db=connection.db(DB);
+        
+        let teachers=await db.collection("teachers").find().toArray();
+        
+        res.json(teachers) 
+
+        await connection.close();
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong,please try again"})
+    }
+})
+
+app.post("/createteacher",async(req,res)=>{
+    try {
+       
         let connection=await mongoclient.connect(URL);
 
         let db=connection.db(DB);
 
-        await db.collection("mentor").insertOne(req.body);
+        await db.collection("teachers").insertOne(req.body);
+
+        res.json({message:"Teacher created"});
 
         await connection.close();
-
-        res.json({message:"Mentor created"});
     } catch (error) {
-        res.status(500).json({message:"Something went wrong,try again"})
+        res.status(500).json({message:"Somethig went wrong,try again"})
     }
 })
 
-
-app.put("/assignmentor/:id",async(req,res)=>{
+app.get("/teacher/:id",async(req,res)=>{
     try {
-        let connection=await mongoclient.connect(URL);
+        let connection=await mongoclient.connect(URL)
 
-        let db=connection.db(DB);
+        let db=connection.db(DB)
 
-        await db.collection("student").findOneAndUpdate({stid:req.params.id},{$set:req.body})
+        let teacher=await db.collection("teachers").findOne({_id:mongodb.ObjectId(req.params.id)})
+        
+        let students=await db.collection("students").find({teacher:teacher.teacher_id}).toArray()
 
-        await connection.close();
+        res.json({teacher,students})
 
-        res.json({message:"Mentor assigned"});
+        await connection.close()
     } catch (error) {
-        res.status(500).json({message:"Something went wrong,try again"})
+        res.status(500).json({message:"Somethig went wrong,try again"})
     }
 })
 
-app.put("/assignstudent/:id",async(req,res)=>{
+app.get("/editteacher/:id",async(req,res)=>{
     try {
+        let connection=await mongoclient.connect(URL)
+
+        let db=connection.db(DB)
+
+        let teacher=await db.collection("teachers").findOne({_id:mongodb.ObjectId(req.params.id)})
         
-        let connection= await mongoclient.connect(URL);
+        res.json(teacher)
 
-        let db=connection.db(DB);
-
-        let available=await db.collection("student").findOne({$and:[{stid:req.body.studentid},{mentor:{$exists:false}}]})
-        
-        if(available){
-        await db.collection("mentor").findOneAndUpdate({mtid:req.params.id},{$push:{students:req.body.studentid}})
-
-        await connection.close();
-
-        res.json({message:"Student added"});
-        }else{
-            res.json({message:"Student already has a mentor"})
-        }
+        await connection.close()
     } catch (error) {
-
-        res.status(500).json({messsage:"Something went wrong,try again"})
+        res.status(500).json({message:"Somethig went wrong,try again"})
     }
 })
 
-app.get("/mentor/:id",async(req,res)=>{
+app.put("/teacher/:id",async(req,res)=>{
     try {
+        let connection=await mongoclient.connect(URL)
+
+        let db=connection.db(DB)
+
+        await db.collection("teachers").findOneAndUpdate({_id:mongodb.ObjectId(req.params.id)},{$set:req.body})
         
-        let connection=await mongoclient.connect(URL);
+        res.json({message:"Teacher details updated"})
 
-        let db=connection.db(DB);
-
-        let students=await db.collection("student").find({mentor:req.params.id},{name : 0,stid : 0}).toArray();
-
-        await connection.close();
-
-        res.json(students);
+        await connection.close()
     } catch (error) {
-        res.status(500).json({message:"Something went wrong,try again"});
+        res.status(500).json({message:"Somethig went wrong,try again"})
+    }
+})
+
+app.delete("/teacher/:id",async(req,res)=>{
+    try {
+        let connection=await mongoclient.connect(URL)
+
+        let db=connection.db(DB)
+
+        await db.collection("teachers").findOneAndDelete({_id:mongodb.ObjectId(req.params.id)})
+        
+        res.json({message:"Teacher details deleted"})
+
+        await connection.close()
+    } catch (error) {
+        res.status(500).json({message:"Somethig went wrong,try again"})
     }
 })
 
 
-
-
-
-
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3001)
